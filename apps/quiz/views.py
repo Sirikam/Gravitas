35565cf77bbe8c7170f034cc882a55e2987c33eb
Sirikam <ben.dik@live.no>
2017-02-19 21:05:23 +0100

    adds urls to quiz project

--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def index(request):
     return HttpResponse("<h1>A page for the quiz app</h1>")
 
-#def create(request):
- #   return render(request, "quiz/proffesor.html")
+def create(request):
+    template = loader.get_template("../templates/quiz/proffesor.html")
+    return HttpResponse(template.render())
 
-#def use(request):
- #   return render(request, "quiz/student.html")
+def use(request):
+    template = loader.get_template("../templates/quiz/student.html")
+    return HttpResponse(template.render())
 
 #def admin(request):
- #   return render(request, "quiz/admin.html")
+    template = loader.get_template("../templates/quiz/admin.html")
+    return HttpResponse(template.render())
