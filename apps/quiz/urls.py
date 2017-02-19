7ca257f8e7872cb9d5263ca8955fa989a96154e8
Sirikam <ben.dik@live.no>
2017-02-19 18:48:19 +0100

    adds some more to quiz app

--- apps/quiz/urls.py
+++ apps/quiz/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index,name='index'),
+    url(r'^$', views.index, name='index'),
+   # url(r'^professor/$', create, name='create'),
+  #  url(r'^student/$', use, name='use'),
+   # url(r'^admin/$', admin, name='admin'),
 ]
--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>A page for the quiz app</h1>")
+
+#def create(request):
+ #   return render(request, "quiz/proffesor.html")
+
+#def use(request):
+ #   return render(request, "quiz/student.html")
+
+#def admin(request):
+ #   return render(request, "quiz/admin.html")
