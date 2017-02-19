1eb4aa10c9bfa2dbfcd138b2fbe2745ad01dc61d
steffhe <steffhe@stud.ntnu.no>
2017-02-19 20:25:36 +0100

    Changed static directory arrangement. Changed main html and css.

--- apps/staticpages/views.py
+++ apps/staticpages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def index(request):
-    return HttpResponse("<h1>THis is the homepage</h1>")
+    template = loader.get_template("../templates/staticpages/main.html")
+    return HttpResponse(template.render())
