b9c0141ad8fc20a27000c20b3233b4e22d0f71af
steffhe <steffhe@stud.ntnu.no>
2017-02-20 14:39:35 +0100

    Merge branch 'master' of C:\Users\Eier\Gravitas with conflicts.

--- apps/staticpages/views.py
+++ apps/staticpages/views.py
@@ -4,13 +4,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.views.generic import TemplateView
-from django.views import View
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
 
 def index(request):
-    return render_to_response('staticpages/main.html')
+    template = loader.get_template("../templates/staticpages/main.html")
+    return HttpResponse(template.render())
 
+class home(TemplateView):
+    template = loader.get_template("../templates/staticpages/main.html")
 
-
+def login(request):
+    template = loader.get_template("../templates/staticpages/login.html")
+    return HttpResponse(template.render())
