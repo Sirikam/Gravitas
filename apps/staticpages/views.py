47f3d5a935f091e038a1db2c19d4acba756227f0
Sirikam <ben.dik@live.no>
2017-02-20 14:36:51 +0100

    creates a homepage setting

--- apps/staticpages/views.py
+++ apps/staticpages/views.py
@@ -4,10 +4,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.views.generic import TemplateView
+from django.views import View
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 
 def index(request):
-    template = loader.get_template("../templates/staticpages/main.html")
-    return HttpResponse(template.render())
+    return render_to_response('staticpages/main.html')
 
-class home(TemplateView):
-    template = loader.get_template("../templates/staticpages/main.html")
+
+
