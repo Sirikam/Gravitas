c46d8a21eda54b390c597d32f5c291103b4ad110
Sirikam <ben.dik@live.no>
2017-02-21 22:51:49 +0100

    adds asuperuser, adds a model too the database, and adds teh possibility too add persons to the database

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class person(AbstractUser):
-    first_name = models.CharField(50)
-    middle_name = models.CharField(50)
-    last_name = models.CharField(50)
-    email = models.CharField(150)
-    professor = bool()
+class person(models.Model):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=150)
+    professor = bool(1)
+
+    def __str__(self):
+        return  self.first_name + ' ' + self.last_name
+
