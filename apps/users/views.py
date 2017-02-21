fcd8305042c7c0e9036578b52c289d0761868b7c
Sirikam <ben.dik@live.no>
2017-02-21 23:17:28 +0100

    more work on usermodels, + update to .gitignore file

--- apps/users/views.py
+++ apps/users/views.py
@@ -7,3 +7,6 @@ from django.template import Context, loader
 def index(request):
     template = loader.get_template("../templates/users/main.html")
     return HttpResponse(template.render())
+
+def detail(request, person_id):
+    return HttpResponse("<h2>details for user_id: " + str(person_id)+ "</h2>")
