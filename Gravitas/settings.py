03d79b716bb0aad69188e3c6b3d340e716ea511b
Sirikam <ben.dik@live.no>
2017-02-22 01:18:58 +0100

    final work on inital database setup, can now succesully add quizes and questions, still work to do on answers

--- Gravitas/settings.py
+++ Gravitas/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.quiz.apps.QuizConfig',
     'apps.users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from django.template import Context, loader
+from .models import Quiz, Question, Answer
+
 
 def index(request):
-    template = loader.get_template("../templates/quiz/main.html")
+    template = loader.get_template("../templates/users/main.html")
     return HttpResponse(template.render())
 
 def create(request):
