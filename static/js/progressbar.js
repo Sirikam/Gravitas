9fdb91516d4f8ef72434d1ca479b48d8f43ccc63
Siri Ulltveit-Moe <siriulltveitmoe@gmail.com>
2017-03-09 16:37:45 +0100

    progressbar

---
+++ static/js/progressbar.js
@@ -0,0 +1,16 @@
+/**
+ * Created by SiriUlltveitMoe on 09/03/17.
+ */
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
