874ce1c659099e0c422fb76687df96b24fa9150e
steffhe <steffhe@stud.ntnu.no>
2017-03-13 15:57:26 +0100

    css changes

--- static/js/rating.js
+++ static/js/rating.js
@@ -5,16 +5,13 @@ $(document).ready(function(){
         $('.rating span').removeClass('checked');
         $(this).parent().addClass('checked');
         var userRating = this.value;
-        var sendt = ('You have given this quiz a' + userRating + 'out of 5. &#10; Takk for din rating.');
+        var sendt = 'You have given this quiz a' + userRating + 'out of 5. &#10; Takk for din rating.';
         $('#takk').innerHTML = sendt;
         $('#takk').show();
     });
 
     $('input:radio').change(
     function(){
-
         $('.rating span').removeClass('checked');
-
-
     });
 });
