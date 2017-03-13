cb0c3f2ec21002c34e43eea95c3586a9e006b37a
steffhe <steffhe@stud.ntnu.no>
2017-03-13 14:51:48 +0100

    hompage css

--- static/js/rating.js
+++ static/js/rating.js
@@ -4,15 +4,17 @@ $(document).ready(function(){
     $('.rating input').click(function () {
         $('.rating span').removeClass('checked');
         $(this).parent().addClass('checked');
+        var userRating = this.value;
+        var sendt = ('You have given this quiz a' + userRating + 'out of 5. &#10; Takk for din rating.');
+        $('#takk').innerHTML = sendt;
+        $('#takk').show();
     });
 
     $('input:radio').change(
     function(){
-        var userRating = this.value;
-        var sendt = ('You have given this quiz a' + userRating + 'out of 5. &#10; Takk for din rating.');
+
         $('.rating span').removeClass('checked');
-        $('#takk').innerHTML = sendt;
-        $('#takk').show();
+
 
     });
 });
