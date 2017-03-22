10e43c738bfdfe9dcab39389a404481c78fc0385
truslaus <juliedavidsen@hotmail.com>
2017-03-22 14:54:25 +0100

    har prøvd på testing

--- apps/quiz/tests.py
+++ apps/quiz/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from apps.quiz.models import Quiz
+from apps.quiz.models import Question
+from apps.quiz.models import Answer
 
-# Create your tests here.
+
+#class QuizTest(TestCase):
+    #def setUp(self):
+
+         #def test_quiz(self):
+       # Quiz.objects.create(name="jususquiztus")
+       # jesus = Quiz.objects.get(name="jesusquiztus")
+       # self.assertEqual(jesus.speak(), 'nytt quiznavn "jesus"')
+
+    #def test_question(self):
+
+    #ef test_answer(self):
--- apps/staticpages/tests.py
+++ apps/staticpages/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from unittest import TestCase
+from django.test import TestCase, RequestFactory
 
-# Create your tests here.
+from django.http import HttpResponse
+from django.http import response
+from django.test import Client
+from apps import staticpages
+#from views import StaticView
+
+
+c = Client()
+c.get('/staticpages/')
+
+
+class GreetingsTest(TestCase):
+
+    def test_greeting(self):
+        self.assertRaises(Exception)
+       # self.assertContains(response, "New<br></br>Lines", count=None, status_code=202, msg_prefix='', html=True)
+        self.assertHTMLEqual("<h1>welcome<\h1>", "<h1>welcome<\h1>", msg='')
+
+   # def test_get(self):
