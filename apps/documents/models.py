893b6950093fb7784aa05b18ceddc3a7a092ed8a
Sirikam <ben.dik@live.no>
2017-03-22 14:32:20 +0100

    links documents to users and courses, as well as making them private or public. might create errors with the existing upload function

--- apps/documents/models.py
+++ apps/documents/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
+from apps.courses.models import Course
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Document(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,default='')
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+    name =  models.CharField(max_length=150, default='')
+    private = models.BooleanField(1, default=1)
+
