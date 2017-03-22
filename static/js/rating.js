00161f368a83fc813c7172eb86d08cfa6351eb78
steffhe <steffhe@stud.ntnu.no>
2017-03-22 15:53:13 +0100

    css

--- static/js/rating.js
+++ static/js/rating.js
@@ -11,8 +11,6 @@ $(document).ready(function(){
         var userRating = this.value;
         var sendt = ('You have given this quiz a' + userRating + 'out of 5. &#10; Takk for din rating.');
         $('.rating span').removeClass('checked');
-        $('#takk').innerHTML = sendt;
-        $('#takk').show();
 
     });
 });
