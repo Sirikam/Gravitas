de3a16156fc69cb7c2f22973f984ce765d779d11
truslaus <juliedavidsen@hotmail.com>
2017-03-23 15:40:36 +0100

    endret forsideiconer

--- apps/documents/tests.py
+++ apps/documents/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from apps.courses.models import Course
+from django.contrib.auth.models import User
+from apps.documents.models import Document
 
-# Create your tests here.
+#class DocumentsTest(TestCase):
+
+    #def test_dokument(self):
