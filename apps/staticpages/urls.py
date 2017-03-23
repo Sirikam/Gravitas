e057224ffdc90ae0e19fbc8bc8f92ad6d17e1f13
steffhe <steffhe@stud.ntnu.no>
2017-03-23 13:35:29 +0100

    added schedule functionality

--- apps/staticpages/urls.py
+++ apps/staticpages/urls.py
@@ -7,5 +7,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^login/$', StaticView.as_view(template_name='staticpages/login.html'), name='login'),
-    url(r'^homepage/$', StaticView.as_view(template_name='staticpages/homepage.html'), name='homepage')
+    url(r'^homepage/$', StaticView.as_view(template_name='staticpages/homepage.html'), name='homepage'),
+    url(r'^schedule/$', StaticView.as_view(template_name='staticpages/schedule.html'), name='schedule')
 ]
