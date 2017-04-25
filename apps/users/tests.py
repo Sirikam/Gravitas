1fc56c6571a2931ad98766d304495c335d42bedc
steffhe <steffhe@stud.ntnu.no>
2017-04-25 14:09:36 +0200

    added quiz, added document, changed documents css/html

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -3,6 +3,7 @@ import inspect
 from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
 from .forms import *
+from django.test import Client
 
 
 """class User_Form_Test(TestCase):
@@ -17,3 +18,18 @@ from .forms import *
         self.assertTrue(form.is_valid())
 """
 
+class PersonTest(TestCase):
+    def setUp(self):
+        self.ole = Person.objects.create(first_name='Ole', last_name='Nordmann', middle_name=' ', PERSON_ROLE='ST')
+        self.kari = Person.objects.create(first_name='Kari', last_name='Nordmann', middle_name=' ', PERSON_ROLE='ST')
+
+    def test_kari_first_name(self):
+        self.assertEqual(self.alice.first_name, 'Kari')
+
+    def test_ole_first_name(self):
+        self.assertEqual(self.bob.first_name, 'Ole')
+
+    def test_ole_first_name_modified(self):
+        self.ole.first_name = 'Truls'
+        self.ole.save()
+        self.assertEqual(self.ole.first_name, 'Truls')
